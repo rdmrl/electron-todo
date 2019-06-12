ed915ad04b05e94c562bbb520b5ef94dfc4ab304
Ravi Damarla <ravi.damarla@gmail.com>
2019-06-12 17:43:55 +0530

    tweaks

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+'use strict';
+
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
